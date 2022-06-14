dee8c4f9a19cf1ccff2c681ca74b4e5005efb41d
Anton <kiyananton@gmail.com>
2022-06-14 17:02:11 +0300

    edits

--- source/js/spend-points.js
+++ source/js/spend-points.js
@@ -1,103 +1,177 @@
-// $(window).on("load", function () {
-//   let $totalPointsBar = $(".spend-points__bar");
-//   let $currentPointsBar = $(".spend-points__bar-current");
-//   let $currentPointsBarText = $(".spend-points__bar-current-text span");
-//   let $limitPoinsBar = $(".spend-points__bar-limit");
+// let $totalPointsBar = $(".spend-points__bar");
+// let $currentPointsBar = $(".spend-points__bar-current");
+// let $currentPointsBarText = $(".spend-points__bar-current-text span");
+// let $limitPoinsBar = $(".spend-points__bar-limit");
 
-//   let $currentPoints = $("#spend-points-current");
-//   let $totalPoints = $("#spend-points-total");
-//   let $limitPoints = $("#spend-points-limit");
-//   let $pointsLeft = $(".spend-points__balance span");
+// let $currentPoints = $("#spend-points-current");
+// let $totalPoints = $("#spend-points-total");
+// let $limitPoints = $("#spend-points-limit");
+// let $pointsLeft = $(".spend-points__balance span");
 
-//   let $inputBlock = $(".input-block.points");
-//   let $minusBtn = $(".spend-points__minus");
-//   let $plusBtn = $(".spend-points__plus");
-//   let $input = $('input[name="points-amount"]');
+// let $inputBlock = $(".input-block.points");
+// let $minusBtn = $(".spend-points__minus");
+// let $plusBtn = $(".spend-points__plus");
+// let $input = $('input[name="points-amount"]');
 
-//   let $submitBtn = $('#spend-points button[type="submit"]');
+// let $submitBtn = $('#spend-points button[type="submit"]');
 
-//   const getCurrentBar = () => {
-//     return Math.ceil((+$input.val() / +$totalPoints.text()) * 1000) / 10;
-//   };
+// const getCurrentBar = () => {
+//   return Math.ceil((+$input.val() / +$totalPoints.text()) * 1000) / 10;
+// };
 
-//   const getLimitBar = () => {
-//     return Math.ceil((+$limitPoints.text() / +$totalPoints.text()) * 100);
-//   };
+// const getLimitBar = () => {
+//   return Math.ceil((+$limitPoints.text() / +$totalPoints.text()) * 100);
+// };
 
-//   const changeInput = () => {
-//     if (!isNaN(+$input.val()) && +$input.val() >= 0) {
-//       if (+$input.val() <= +$limitPoints.text()) {
-//         $currentPointsBar.css("width", `${getCurrentBar()}%`);
-//         $currentPointsBarText.text($input.val());
-//         $currentPoints.text($input.val().trim() === "" ? 0 : $input.val());
-//         $inputBlock.removeClass("error");
-//         $submitBtn.removeAttr("disabled");
-
-//         $pointsLeft.text(+$totalPoints.text() - +$currentPoints.text());
-//       } else {
-//         $currentPointsBar.css("width", `${getLimitBar()}%`);
-//         $currentPointsBarText.text($limitPoints.text());
-//         $currentPoints.text($limitPoints.text());
-//         $inputBlock.addClass("error");
-//         $submitBtn.attr("disabled", true);
-
-//         $pointsLeft.text(+$limitPoints.text() - +$currentPoints.text());
-//       }
-//     } else {
-//       $inputBlock.addClass("error");
-//       $submitBtn.attr("disabled", true);
-//       $currentPointsBarText.text(0);
-//       $currentPoints.text(0);
-//       $currentPointsBar.css("width", `0%`);
+// const changeInput = () => {
+//   if (!isNaN(+$input.val()) && +$input.val() >= 0) {
+//     if (+$input.val() <= +$limitPoints.text()) {
+//       $currentPointsBar.css("width", `${getCurrentBar()}%`);
+//       $currentPointsBarText.text($input.val());
+//       $currentPoints.text($input.val().trim() === "" ? 0 : $input.val());
+//       $inputBlock.removeClass("error");
+//       $submitBtn.removeAttr("disabled");
 
 //       $pointsLeft.text(+$totalPoints.text() - +$currentPoints.text());
-//     }
-//   };
-
-//   console.log(getLimitBar());
-
-//   $input.on("input", changeInput);
-
-//   $plusBtn.on("click", function () {
-//     if (isNaN(+$input.val())) {
-//       $input.val(0);
-//     }
-
-//     $input.val(`${+$input.val() + 1}`);
-
-//     changeInput();
-//   });
-
-//   $minusBtn.on("click", function () {
-//     if (isNaN(+$input.val())) {
-//       $input.val(1);
-//     }
-
-//     if (+$input.val() - 1 < 0) {
-//       $input.val(0);
 //     } else {
-//       $input.val(`${+$input.val() - 1}`);
+//       $currentPointsBar.css("width", `${getLimitBar()}%`);
+//       $currentPointsBarText.text($limitPoints.text());
+//       $currentPoints.text($limitPoints.text());
+//       $inputBlock.addClass("error");
+//       $submitBtn.attr("disabled", true);
+
+//       $pointsLeft.text(+$limitPoints.text() - +$currentPoints.text());
 //     }
+//   } else {
+//     $inputBlock.addClass("error");
+//     $submitBtn.attr("disabled", true);
+//     $currentPointsBarText.text(0);
+//     $currentPoints.text(0);
+//     $currentPointsBar.css("width", `0%`);
 
-//     changeInput();
-//   });
+//     $pointsLeft.text(+$totalPoints.text() - +$currentPoints.text());
+//   }
+// };
 
-//   $(document).on("click", function () {
-//     $totalPointsBar = $(".spend-points__bar");
-//     $currentPointsBar = $(".spend-points__bar-current");
-//     $currentPointsBarText = $(".spend-points__bar-current-text span");
-//     $limitPoinsBar = $(".spend-points__bar-limit");
+// console.log(getLimitBar());
 
-//     $currentPoints = $("#spend-points-current");
-//     $totalPoints = $("#spend-points-total");
-//     $limitPoints = $("#spend-points-limit");
-//     $pointsLeft = $(".spend-points__balance span");
+// $input.on("input", changeInput);
 
-//     $inputBlock = $(".input-block.points");
-//     $minusBtn = $(".spend-points__minus");
-//     $plusBtn = $(".spend-points__plus");
-//     $input = $('input[name="points-amount"]');
+// $plusBtn.on("click", function () {
+//   if (isNaN(+$input.val())) {
+//     $input.val(0);
+//   }
 
-//     $submitBtn = $('#spend-points button[type="submit"]');
-//   });
+//   $input.val(`${+$input.val() + 1}`);
+
+//   changeInput();
 // });
+
+// $minusBtn.on("click", function () {
+//   if (isNaN(+$input.val())) {
+//     $input.val(1);
+//   }
+
+//   if (+$input.val() - 1 < 0) {
+//     $input.val(0);
+//   } else {
+//     $input.val(`${+$input.val() - 1}`);
+//   }
+
+//   changeInput();
+// });
+
+$(document).on("click", function () {
+  const $pointsTrigger = $(".product-card__points a");
+
+  if ($pointsTrigger.attr("data-init")) {
+    console.log("no");
+    return;
+  } else {
+    console.log("yes");
+    let $totalPointsBar = $(".spend-points__bar");
+    let $currentPointsBar = $(".spend-points__bar-current");
+    let $currentPointsBarText = $(".spend-points__bar-current-text span");
+    let $limitPoinsBar = $(".spend-points__bar-limit");
+
+    let $currentPoints = $("#spend-points-current");
+    let $totalPoints = $("#spend-points-total");
+    let $limitPoints = $("#spend-points-limit");
+    let $pointsLeft = $(".spend-points__balance span");
+
+    let $inputBlock = $(".input-block.points");
+    let $minusBtn = $(".spend-points__minus");
+    let $plusBtn = $(".spend-points__plus");
+    let $input = $('input[name="points-amount"]');
+
+    let $submitBtn = $('#spend-points button[type="submit"]');
+
+    const getCurrentBar = () => {
+      return Math.ceil((+$input.val() / +$totalPoints.text()) * 1000) / 10;
+    };
+
+    const getLimitBar = () => {
+      return Math.ceil((+$limitPoints.text() / +$totalPoints.text()) * 100);
+    };
+
+    const changeInput = () => {
+      if (!isNaN(+$input.val()) && +$input.val() >= 0) {
+        if (+$input.val() <= +$limitPoints.text()) {
+          $currentPointsBar.css("width", `${getCurrentBar()}%`);
+          $currentPointsBarText.text($input.val());
+          $currentPoints.text($input.val().trim() === "" ? 0 : $input.val());
+          $inputBlock.removeClass("error");
+          $submitBtn.removeAttr("disabled");
+
+          $pointsLeft.text(+$totalPoints.text() - +$currentPoints.text());
+        } else {
+          $currentPointsBar.css("width", `${getLimitBar()}%`);
+          $currentPointsBarText.text($limitPoints.text());
+          $currentPoints.text($limitPoints.text());
+          $inputBlock.addClass("error");
+          $submitBtn.attr("disabled", true);
+
+          $pointsLeft.text(+$limitPoints.text() - +$currentPoints.text());
+        }
+      } else {
+        $inputBlock.addClass("error");
+        $submitBtn.attr("disabled", true);
+        $currentPointsBarText.text(0);
+        $currentPoints.text(0);
+        $currentPointsBar.css("width", `0%`);
+
+        $pointsLeft.text(+$totalPoints.text() - +$currentPoints.text());
+      }
+    };
+
+    console.log(getLimitBar());
+
+    $input.on("input", changeInput);
+
+    $plusBtn.on("click", function () {
+      if (isNaN(+$input.val())) {
+        $input.val(0);
+      }
+
+      $input.val(`${+$input.val() + 1}`);
+
+      changeInput();
+    });
+
+    $minusBtn.on("click", function () {
+      if (isNaN(+$input.val())) {
+        $input.val(1);
+      }
+
+      if (+$input.val() - 1 < 0) {
+        $input.val(0);
+      } else {
+        $input.val(`${+$input.val() - 1}`);
+      }
+
+      changeInput();
+    });
+
+    $pointsTrigger.attr("data-init", true);
+  }
+});
