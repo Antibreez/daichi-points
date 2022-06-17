a46eaecdde662faeefe0269d161c3bef2b4aa6ac
Anton Kiyan <kiyananton@gmail.com>
2022-06-17 14:39:50 +0300

    edits

--- source/js/slider-tooltip.js
+++ source/js/slider-tooltip.js
@@ -5,22 +5,29 @@ $chip.on("mouseenter", function () {
     return;
   }
 
+  const $tooltipCurrent = $(this).find(".tooltip");
+  const points = $tooltipCurrent.find(".tooltip__title").text();
+  const link = $tooltipCurrent.find("a").attr("href");
+
   const $parent = $(this).parents(".wrap-catalog-slider").first();
   const $tooltip = $parent.find(".tooltip").first();
+  $tooltip.find(".tooltip__title").text(points);
+  $tooltip.find("a").attr("href", link);
 
   const offsetP = $parent.offset();
   const offset = $(this).offset();
 
+  console.log("left", offset.left - offsetP.left - Math.round($(this).width() / 2));
+
+  const leftGap = offset.left - offsetP.left - Math.round($(this).width() / 2);
+
+  const leftDist = leftGap <= 10 ? 10 - leftGap : 0;
+
   $tooltip.css({
     top: `${offset.top - offsetP.top}px`,
-    left: `${offset.left - offsetP.left + Math.round($(this).width() / 2)}px`,
+    left: `${offset.left - offsetP.left + Math.round($(this).width() / 2) + leftDist}px`,
     display: "block",
   });
-
-  console.log("over");
-  console.log($(this).width());
-  console.log(offset.top - offsetP.top);
-  console.log(offset.left - offsetP.left);
 });
 
 $chip.on("mouseleave", function () {
