afb52a650cc544d21387a64ff281c5f6dc2fbca5
Anton <kiyananton@gmail.com>
2022-09-26 16:05:00 +0300

    edits

--- source/js/main.js
+++ source/js/main.js
@@ -9,7 +9,7 @@ import "./date";
 
 import "./education-promo-slider";
 import "./header-bar";
-import "./categories";
+//import "./categories";
 import "./advantages";
 import "./lk-tabs";
 import "./certificates-slider";
