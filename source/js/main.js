639790eb7dbed506f97d708a42a12a4b0a7820c2
Anton <kiyananton@gmail.com>
2022-09-27 15:43:57 +0300

    edits

--- source/js/main.js
+++ source/js/main.js
@@ -1,21 +1,21 @@
 import "./vendor/jquery-ui";
 
-import "./modal";
-import "./scroll";
-import "./search";
+// import "./modal";
+// import "./scroll";
+// import "./search";
 import "./select";
 import "./filters";
 import "./date";
 
-import "./education-promo-slider";
-import "./header-bar";
+//import "./education-promo-slider";
+//import "./header-bar";
 //import "./categories";
-import "./advantages";
-import "./lk-tabs";
-import "./certificates-slider";
-import "./filter-slider";
-import "./program-card";
-import "./testing";
+//import "./advantages";
+//import "./lk-tabs";
+//import "./certificates-slider";
+//import "./filter-slider";
+//import "./program-card";
+//import "./testing";
 import "./program-reasons";
 import "./points-questions";
 import "./spend-points";
